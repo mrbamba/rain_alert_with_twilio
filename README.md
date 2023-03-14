74d6c64cfc0d367c6eb70a502d32fa1339bf28f7
Dan Haski <dhaski@gmail.com>
2023-03-14 11:51:00 +0200

    Project ready

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rain alert with Twilio and OpenWeatherMaps API
+# Setup:
+# -----run the below comments in terminal once to set up variables using your own API keys-----
+# echo "export TWILIO_ACCOUNT_SID='your_twilio_sid_goes_here'" > twilio.env
+# echo "export TWILIO_AUTH_TOKEN='your_twilio auth_token_goes_here'" >> twilio.env
+# echo "export OWM_API_KEY='your_open_weather_maps_api_key_goes_here'" >> twilio.env
+# source ./twilio.env
+# echo "twilio.env" >> .gitignore
+
+# ----- EXAMPLE twilio.env FILE: -----
+# export TWILIO_ACCOUNT_SID='ACoiejrgoih345t3goij3goij3foij3foij34'
+# export TWILIO_AUTH_TOKEN='cosndvlkjnsvnwronwerovsd280werwt4'
+# export OWM_API_KEY='dfo340jw0e98fwe09j230jf20jf230f9j2'
---
+++ main.py
@@ -0,0 +1,50 @@
+import requests
+import os
+from twilio.rest import Client
+# The below import imports load_dotenv which will go over to twilio.env file and import its variables into os.environ
+from dotenv import load_dotenv
+
+
+# Loads variables into os.environ from external file
+load_dotenv('twilio.env')
+
+
+# Load Twilio Keys from os.environ
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+print(account_sid, auth_token)
+
+
+# Load OpenWeatherMap key from os.environ
+owm_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+OWM_api_key = os.environ.get('OWM_API_KEY')
+
+# ----------- MAKE SURE TO USE THE CORRECT LAT/LANG BELOW
+parameters = {
+    "lat":  32.6207,
+    "lon": 35.5521,
+    "exclude": "current,minutely,daily",
+    "appid": OWM_api_key,
+}
+response = requests.get(owm_endpoint, params=parameters)
+response.raise_for_status()
+print(response.status_code)
+hourly = response.json()["hourly"][:16]
+send_alert = False
+
+for hour_data in hourly:
+    print("running")
+    if hour_data["weather"][0]["id"] < 700:
+        send_alert = True
+        break
+
+if send_alert:
+    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body='Bring an umbrella☔️',
+        from_='+YOU_TWILIO_NUMBER',
+        to='+RECIPIENT_PHONE_NUMBER'
+    )
+
+    print(message.status)
